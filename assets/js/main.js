82aa2ebd2781658f2d42a3ac3fbaf54a848e60f4
NISHAN75 <nishandas880@gmail.com>
2025-01-10 14:25:26 +0600

    all is ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,39 @@
     }
     reverseCardRotate();
 
-  
+  		// Magnific popup
+          $('.trigger-popup').magnificPopup({
+			type: 'iframe',
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+					'<div class="mfp-close"></div>' +
+					'<iframe class="mfp-iframe" frameborder="0" allowfullscreen allow="autoplay *; fullscreen *"></iframe>' +
+					'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: function (url) {
+							var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+							if (!m || !m[1]) return null;
+							return m[1];
+						},
+						src: '//www.youtube.com/embed/%id%?autoplay=1&iframe=true'
+					},
+					vimeo: {
+						index: 'vimeo.com/',
+						id: function (url) {
+							var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+							if (!m || !m[5]) return null;
+							return m[5];
+						},
+						src: '//player.vimeo.com/video/%id%?autoplay=1'
+					}
+				}
+			},
+		});
+        $(document).on('mfpOpen', function () {
+            $(".mfp-close").text("X");
+        });
         // portfolio slider
         let portfolioSlider = new Swiper(".portfolio-slider", {
             slidesPerView: 7,
